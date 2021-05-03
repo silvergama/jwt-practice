3d966102803ba012f23f9a97f1bb215ebd8655c1
Silver Gama <silver.mdg@gmail.com>
2021-05-03 09:02:54 -0300

    Add basic struct

---
+++ go.mod
@@ -0,0 +1,10 @@
+module jwt-practice
+
+go 1.16
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/mattn/go-sqlite3 v1.14.7
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b
+)
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+type User struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Role     string `json:"role"`
+}
+
+type Authentication struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Token struct {
+	Role        string `json:"role"`
+	Email       string `json:"email"`
+	TokenString string `json:"tokenString"`
+}
