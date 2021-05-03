344482ff8b2e09728fdabd22deb25bffea87098d
Silver Gama <silver.mdg@gmail.com>
2021-05-03 09:05:54 -0300

    Add database connection

--- main.go
+++ main.go
@@ -17,3 +17,15 @@ type Token struct {
 	Email       string `json:"email"`
 	TokenString string `json:"tokenString"`
 }
+
+func GetDatabase() *sql.DB {
+
+	db, err := sql.Open("sqlite3", "data/jwt.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Conected to Database")
+
+	return db
+}
