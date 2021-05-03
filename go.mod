f331153b9c9d2864e7339f8a254fc12007e6c892
Silver Gama <silver.mdg@gmail.com>
2021-05-03 09:23:25 -0300

    Add Signup route

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.8.0
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b
 )
--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
 	"golang.org/x/crypto/bcrypt"
 )
 
-var secretkey string = "secretkeyJWT"
+var (
+	router    *mux.Router
+	secretkey string = "secretkeyJWT"
+)
 
 type User struct {
 	Name     string `json:"name"`
@@ -30,23 +36,6 @@ type Token struct {
 	TokenString string `json:"tokenString"`
 }
 
-func GetDatabase() *sql.DB {
-
-	db, err := sql.Open("sqlite3", "data/jwt.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Conected to Database")
-
-	return db
-}
-
-func GenerateHashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-	return string(bytes), err
-}
-
 func GenerateJWT(email, role string) (string, error) {
 	var mySigningKey = []byte(secretkey)
 	token := jwt.New(jwt.SigningMethodHS256)
@@ -64,4 +53,51 @@ func GenerateJWT(email, role string) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+func CreateRouter() {
+	router = mux.NewRouter()
+}
+
+func InitializeRoute() {
+	router.HandleFunc("/signup", SignUp).Methods("POST")
+}
+
+func SignUp(w http.ResponseWriter, r *http.Request) {
+	db := GetDatabase()
+	defer db.Close()
+
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	user.Password, err = GenerateHashPassword(user.Password)
+	if err != nil {
+		log.Fatalln("error in password hash")
+	}
+
+	// TODO save new user
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+
+}
+
+func GetDatabase() *sql.DB {
+	db, err := sql.Open("sqlite3", "data/jwt.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Conected to Database")
+
+	return db
+}
+
+func GenerateHashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
 }
